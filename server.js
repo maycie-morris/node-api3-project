0db33ed9bc5463e234fb17e7da74a7fe4642b393
Maycie Morris <maycie.morris@gmail.com>
2020-10-01 11:58:19 -0400

    Middleware functions created

--- server.js
+++ server.js
@@ -8,6 +8,28 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  let currentDate = new Date();
+  let formattedDate =
+    current_datetime.getFullYear() +
+    "-" +
+    (current_datetime.getMonth() + 1) +
+    "-" +
+    current_datetime.getDate() +
+    " " +
+    current_datetime.getHours() +
+    ":" +
+    current_datetime.getMinutes() +
+    ":" +
+    current_datetime.getSeconds();
+    let method = req.method;
+    let url = req.url;
+    let status = res.statusCode;
+    let log = `[${formattedDate}] ${method}:${url} ${status}`
+    console.log(log)
+    next()  
+}
+
+server.use(logger)
 
 module.exports = server;
